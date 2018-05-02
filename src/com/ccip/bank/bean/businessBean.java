0f1b7d467a545fde9b2d18c6ffa66a44600afbdc
lihanghang <lhhroots@126.com>
2018-05-02 11:16:57 +0800

    update fxpg model

---
+++ src/com/ccip/bank/bean/businessBean.java
@@ -0,0 +1,212 @@
+package com.ccip.bank.bean;
+/**
+ * @date 2018年5月2日 上午10:37:40 
+ */
+/**
+ * @author Mason
+ *
+ */
+public class businessBean {
+	
+	private double netSaleRate;
+	private double totalAssetRewardRate;
+	private double netAssetYield;
+	private double inventoryTurnover;
+	private double totalAssetTurnover;
+	private double costProfitMargin;
+	private int litigationNum;
+	private int abOperNum;
+	private int adminSancteNum;
+	private int chattelPledgeNum;
+	private int selfRiskNum;
+	private int peripheralRiskNum;
+	private int shareholdNum;
+	private double firstMaxShare;
+	/**
+	 * @return the netSaleRate
+	 */
+	public double getNetSaleRate() {
+		return netSaleRate;
+	}
+	/**
+	 * @param netSaleRate the netSaleRate to set
+	 */
+	public void setNetSaleRate(double netSaleRate) {
+		this.netSaleRate = netSaleRate;
+	}
+	/**
+	 * @return the totalAssetRewardRate
+	 */
+	public double getTotalAssetRewardRate() {
+		return totalAssetRewardRate;
+	}
+	/**
+	 * @param totalAssetRewardRate the totalAssetRewardRate to set
+	 */
+	public void setTotalAssetRewardRate(double totalAssetRewardRate) {
+		this.totalAssetRewardRate = totalAssetRewardRate;
+	}
+	/**
+	 * @return the netAssetYield
+	 */
+	public double getNetAssetYield() {
+		return netAssetYield;
+	}
+	/**
+	 * @param netAssetYield the netAssetYield to set
+	 */
+	public void setNetAssetYield(double netAssetYield) {
+		this.netAssetYield = netAssetYield;
+	}
+	/**
+	 * @return the inventoryTurnover
+	 */
+	public double getInventoryTurnover() {
+		return inventoryTurnover;
+	}
+	/**
+	 * @param inventoryTurnover the inventoryTurnover to set
+	 */
+	public void setInventoryTurnover(double inventoryTurnover) {
+		this.inventoryTurnover = inventoryTurnover;
+	}
+	/**
+	 * @return the totalAssetTurnover
+	 */
+	public double getTotalAssetTurnover() {
+		return totalAssetTurnover;
+	}
+	/**
+	 * @param totalAssetTurnover the totalAssetTurnover to set
+	 */
+	public void setTotalAssetTurnover(double totalAssetTurnover) {
+		this.totalAssetTurnover = totalAssetTurnover;
+	}
+	/**
+	 * @return the costProfitMargin
+	 */
+	public double getCostProfitMargin() {
+		return costProfitMargin;
+	}
+	/**
+	 * @param costProfitMargin the costProfitMargin to set
+	 */
+	public void setCostProfitMargin(double costProfitMargin) {
+		this.costProfitMargin = costProfitMargin;
+	}
+	/**
+	 * @return the litigationNum
+	 */
+	public int getLitigationNum() {
+		return litigationNum;
+	}
+	/**
+	 * @param litigationNum the litigationNum to set
+	 */
+	public void setLitigationNum(int litigationNum) {
+		this.litigationNum = litigationNum;
+	}
+	/**
+	 * @return the abOperNum
+	 */
+	public int getAbOperNum() {
+		return abOperNum;
+	}
+	/**
+	 * @param abOperNum the abOperNum to set
+	 */
+	public void setAbOperNum(int abOperNum) {
+		this.abOperNum = abOperNum;
+	}
+	/**
+	 * @return the adminSancteNum
+	 */
+	public int getAdminSancteNum() {
+		return adminSancteNum;
+	}
+	/**
+	 * @param adminSancteNum the adminSancteNum to set
+	 */
+	public void setAdminSancteNum(int adminSancteNum) {
+		this.adminSancteNum = adminSancteNum;
+	}
+	/**
+	 * @return the chattelPledgeNum
+	 */
+	public int getChattelPledgeNum() {
+		return chattelPledgeNum;
+	}
+	/**
+	 * @param chattelPledgeNum the chattelPledgeNum to set
+	 */
+	public void setChattelPledgeNum(int chattelPledgeNum) {
+		this.chattelPledgeNum = chattelPledgeNum;
+	}
+	/**
+	 * @return the selfRiskNum
+	 */
+	public int getSelfRiskNum() {
+		return selfRiskNum;
+	}
+	/**
+	 * @param selfRiskNum the selfRiskNum to set
+	 */
+	public void setSelfRiskNum(int selfRiskNum) {
+		this.selfRiskNum = selfRiskNum;
+	}
+	/**
+	 * @return the peripheralRiskNum
+	 */
+	public int getPeripheralRiskNum() {
+		return peripheralRiskNum;
+	}
+	/**
+	 * @param peripheralRiskNum the peripheralRiskNum to set
+	 */
+	public void setPeripheralRiskNum(int peripheralRiskNum) {
+		this.peripheralRiskNum = peripheralRiskNum;
+	}
+	/**
+	 * @return the shareholdNum
+	 */
+	public int getShareholdNum() {
+		return shareholdNum;
+	}
+	/**
+	 * @param shareholdNum the shareholdNum to set
+	 */
+	public void setShareholdNum(int shareholdNum) {
+		this.shareholdNum = shareholdNum;
+	}
+	/**
+	 * @return the firstMaxShare
+	 */
+	public double getFirstMaxShare() {
+		return firstMaxShare;
+	}
+	/**
+	 * @param firstMaxShare the firstMaxShare to set
+	 */
+	public void setFirstMaxShare(double firstMaxShare) {
+		this.firstMaxShare = firstMaxShare;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "businessBean [netSaleRate=" + netSaleRate
+				+ ", totalAssetRewardRate=" + totalAssetRewardRate
+				+ ", netAssetYield=" + netAssetYield + ", inventoryTurnover="
+				+ inventoryTurnover + ", totalAssetTurnover="
+				+ totalAssetTurnover + ", costProfitMargin=" + costProfitMargin
+				+ ", litigationNum=" + litigationNum + ", abOperNum="
+				+ abOperNum + ", adminSancteNum=" + adminSancteNum
+				+ ", chattelPledgeNum=" + chattelPledgeNum + ", selfRiskNum="
+				+ selfRiskNum + ", peripheralRiskNum=" + peripheralRiskNum
+				+ ", shareholdNum=" + shareholdNum + ", firstMaxShare="
+				+ firstMaxShare + "]";
+	}
+	
+}
+
