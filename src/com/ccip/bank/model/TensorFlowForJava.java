58e22b231916830edde15c07e2e69c3358b97d23
lhhroots <754086474@qq.com>
2018-08-18 10:27:25 +0800

    update TFModel for Hang-Hang Lee

--- src/com/ccip/bank/model/TensorFlowForJava.java
+++ src/com/ccip/bank/model/TensorFlowForJava.java
@@ -15,46 +15,30 @@ import org.tensorflow.*;
  */
 public class TensorFlowForJava {
 
+public static void main(String[] args) throws FileNotFoundException, IOException {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-//		    SavedModelBundle b = SavedModelBundle.load("D://java-project/enterpriseInfo/TFModel/Risk/", "risk");
-//	        Session tfSession = b.session();
-//	        Operation operationPredict = b.graph().operation("predict");   //要执行的
-//	        Output output = new Output(operationPredict, 0);
-//	        System.out.println(output);
-//	        float[][] a = new float[1][9];
-//	        a[0] = new float[]{0.517647f,0.839216f,0.992157f,0.996078f,0.992157f,0.796079f,0.635294f,0.160784f,0.796079f};
-//	        Tensor input_x = Tensor.create(a);
-//	        float out = tfSession.runner().feed("input_x", input_x).fetch(output).run().get(0).floatValue();
-//	        for (Tensor s : out) {
-//	            float[][] t = new float[1][1];
-//	            s.copyTo(t);
-//	            for (float i : t[0])
-//	                System.out.println(i);
-//	        }
-
-//		
-		        try (Graph graph = new Graph()) {
-		            //导入图
-		            byte[] graphBytes = IOUtils.toByteArray(new FileInputStream("D://java-project/enterpriseInfo/TFModel/Risk/risk.pb"));
-		            graph.importGraphDef(graphBytes);
-		            float[][] a = new float[1][9];
-			        a[0] = new float[]{0.110502605575513f,0.176740518459297f,0.255217838189331f,3.3848964220829f,0.770701674854596f,0.172805926106074f,3.3848964220829f,0.770701674854596f,0.172805926106074f};
-			        float keep = 0.8f;
-		            //根据图建立Session
-		            try(Session sess = new Session(graph)){
-		            	Tensor x = Tensor.create(a);
-		            	Tensor keep_drop = Tensor.create(keep);
-		            	//System.out.println(x);
-		            	List<Tensor<?>> y = sess.runner().feed("input_x", x).feed("Placeholder_1", keep_drop).fetch("output/predict").run();
-//		                //相当于TensorFlow Python中的sess.run(z, feed_dict = {'in_put': 10.0})
-		            	 for (Tensor s : y) {
-		     	            float[][] t = new float[1][1];
-		     	            s.copyTo(t);
-		     	            for (float i : t[0])
-		     	                System.out.println(i);
-		     	        }		         
-		            }
-		        }
-		    }	    		    	
-	    }	    
+        try (Graph graph = new Graph()) {
+            //导入图
+            byte[] graphBytes = IOUtils.toByteArray(new FileInputStream("D://java-project/enterpriseInfo/TFModel/CreditModel/creditGrade.pb"));
+            graph.importGraphDef(graphBytes);
+            float[][][] a = new float[1][1][106];
+            a[0][0] = new float[]{199,0,0,69,204,12,0,484,350,18,0,604,0,0,0,972,1456,0,0,0,51,7,2,3,0,0,63,0,0,20,83,1500,2,0,-129,1373,1373,1456,416,416,5,5,2,58,349,0,0,0,2,1,0,3,0,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0.3f,0.4f,416,0.2f,0.2f,0.2f,0,5.7f,768.3f,749.2f,0,0,98.8f,0.7f,0,0,1.1f,0
+};
+            float keep = 1.0f;
+            //根据图建立Session
+            try(Session sess = new Session(graph)){
+            Tensor x = Tensor.create(a);
+            Tensor  keep_prob = Tensor.create(keep);
+            //System.out.println(x);
+            List<Tensor<?>> y = sess.runner().feed("inputs", x).feed("keep",  keep_prob).fetch("predicted").run();        
+                //相当于TensorFlow Python中的sess.run(z, feed_dict = {'in_put': 10.0})
+            for (Tensor s : y) {
+            	float[][] t = new float[1][1];
+            	s.copyTo(t);
+            	for (float i : t[0])
+                System.out.println("Risk_Index:"+i);
+            	}         
+            }
+        }
+    }
+  }    
