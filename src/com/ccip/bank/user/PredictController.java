cabc1cca6d099508a577212930f2bc066c3c359a
lhhroots <754086474@qq.com>
2018-09-06 15:59:17 +0800

    update

--- src/com/ccip/bank/user/PredictController.java
+++ src/com/ccip/bank/user/PredictController.java
@@ -653,10 +653,8 @@ public class PredictController extends Controller {
 			renderJson(predValue);
 			// System.out.println(predValue);
 		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
